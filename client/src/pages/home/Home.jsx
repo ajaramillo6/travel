fdd7a89765dc37f1abad62d9710cfcdcc72aded6
Alvaro Jaramillo <alvaro_josej@hotmail.com>
2022-06-09 16:20:25 -0400

    fix latest post arrow slider

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -8,6 +8,7 @@ import LatestPosts from "../../components/latestPosts/LatestPosts";
 export default function Home() {
   const[posts, setPosts] = useState([]);
   const[buttonHover, setButtonHover] = useState(false);
+  const [index, setIndex] = useState(0);
   const {search} = useLocation();
 
   useEffect(()=> {
@@ -28,25 +29,44 @@ export default function Home() {
     setButtonHover(false);
   }
 
+  const handleScroll = (direction) => {
+    if(direction === "l") {
+        setIndex(index !== 0 ? index-1 : posts.length-5)
+    }
+    if(direction === "r") {
+        setIndex(index !== posts.length-5 ? index+1 : 0)
+    }
+}
+
   return (
     <>
       <Header />
       <div className="home">
         <span className="homeText">Latest Posts</span>
-        <hr style={{width:'50%', 'textAlign':'center', 'marginLeft':'auto', 'marginRight':'auto', 'marginBottom': '10px'}}></hr>
-        <div className="homePostsContainer">
+        <hr style={{width:'50%', 'textAlign':'center', 'marginLeft':'auto', 'marginRight':'auto', 'marginBottom': '380px'}}></hr>
+        <div className="homePostsContainer" style={{transform:`translateX(${-18.5*index}vw)`}}>
             {posts.slice(0,8).map((post) => (
               <LatestPosts post={post} />
             ))}
         </div>
         <div onMouseOver={handleButton} onMouseLeave={handleButtonLeave}>
-            <Link className="link" to="/travel">
-              <div className="homeReadMore"> 
-                Read More Posts 
-                <i className={buttonHover ? "homePlane fa-solid fa-plane":"homePlaneHide fa-solid fa-plane"}></i>
-              </div>
-            </Link>
+          <Link className="link" to="/travel">
+            <div className="homeReadMore"> 
+              Read More Posts 
+              <i className={buttonHover ? "homePlane fa-solid fa-plane":"homePlaneHide fa-solid fa-plane"}></i>
+            </div>
+          </Link>
         </div>
+        {index > 0 &&
+        <div className="homePostsArrowLeft" style={{left: 0}} onClick = {()=>handleScroll("l")}>
+          <i className="fa-solid fa-angle-left"></i>
+        </div>
+        }
+        {index < posts.length - 5 &&
+        <div className="homePostsArrowRight" style={{right: 0}} onClick = {()=>handleScroll("r")}>
+          <i className="fa-solid fa-angle-right"></i>
+        </div>
+        }
       </div>
     </>
   )
