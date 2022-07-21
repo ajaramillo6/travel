c8b761ae9dc536e9410a069896b22d3983cca7c5
Alvaro Jaramillo <alvaro_josej@hotmail.com>
2022-07-21 00:10:13 -0400

    fix a little issue

--- api/client/src/pages/single/Single.jsx
+++ api/client/src/pages/single/Single.jsx
@@ -1,9 +1,8 @@
-import './single.css';
 import SinglePost from "../../components/singlePost/SinglePost";
 
 export default function Single() {
   return (
-    <div className="single">
+    <div className="single" style={{display:"flex", marginTop: "40px"}}>
         <SinglePost />
     </div>
   )
